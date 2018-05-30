88a6a7340a88941ef3f6dc904b6348a0950d519e
Rafael Alencar <rafael.alencar@easynvest.com.br>
2018-05-30 18:09:43 -0300

    Setup initial project structure

---
+++ TweetEmotionsTests/TweetEmotionsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TweetEmotionsTests.swift
+//  TweetEmotionsTests
+//
+//  Created by Rafael de Castro Santos Alencar on 30/05/18.
+//  Copyright © 2018 rafaelcsa. All rights reserved.
+//
+
+import XCTest
+@testable import TweetEmotions
+
+class TweetEmotionsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
