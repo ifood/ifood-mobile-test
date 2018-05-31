4924cfab1b2bcecf88394ad85677e91d158a9833
Antony Alkmim <antony-alkmim@hotmail.com>
2018-05-31 02:11:08 -0300

    Add basic ui test
    Add tests for basic app navigation

--- TwimotionUITests/TwimotionUITests.swift
+++ TwimotionUITests/TwimotionUITests.swift
@@ -2,7 +2,7 @@
 //  TwimotionUITests.swift
 //  TwimotionUITests
 //
-//  Created by Antony on 26/05/18.
+//  Created by Antony on 30/05/18.
 //  Copyright © 2018 Twimotion. All rights reserved.
 //
 
@@ -12,25 +12,28 @@ class TwimotionUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAppFlow() {
+        
+        let app = XCUIApplication()
+        app.textFields["@username"].tap()
+        app.textFields["@username"].typeText("elonmusk")
+        app.buttons["List tweets"].tap()
+        
+        let tablesQuery = app.tables
+        
+        tablesQuery.cells.element(boundBy: 0).tap() // tap first cell
+        app.buttons["close"].tap()
+        
+        tablesQuery.cells.element(boundBy: 1).tap() // tap second cell
+        app.buttons["close"].tap()
+        
+        app.navigationBars["@elonmusk"].buttons["Back"].tap()
     }
     
 }
