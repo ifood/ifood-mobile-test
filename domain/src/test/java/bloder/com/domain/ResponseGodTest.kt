bf2542afd1c18d1ec8c2b446003417aa66e3d3fe
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-11 11:28:56 -0300

    Create tests for response god

---
+++ domain/src/test/java/bloder/com/domain/ResponseGodTest.kt
@@ -0,0 +1,41 @@
+package bloder.com.domain
+
+import bloder.com.domain.api_response.ResponseGod
+import com.nhaarman.mockitokotlin2.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ResponseGodTest {
+
+    private val responseGod = mock<ResponseGod>()
+
+    @Test
+    fun testOn401Error() {
+        mockResponseGodWith(401)
+        doNothing().whenever(responseGod).unknown(401)
+        responseGod.handle(401)
+        verify(responseGod, times(1)).unknown(401)
+    }
+
+    @Test
+    fun testOn200Response() {
+        mockResponseGodWith(200)
+        doNothing().whenever(responseGod).on200()
+        responseGod.handle(200)
+        verify(responseGod, times(1)).on200()
+    }
+
+    private fun mockResponseGodWith(code: Int) {
+        doAnswer {
+            val map = mockResponseMap()
+            if (map[code] != null) map[code]?.invoke()
+            else responseGod.unknown(code)
+        }.whenever(responseGod).handle(code)
+    }
+
+    private fun mockResponseMap() : Map<Int, () -> Any> = mapOf(
+            200 to { responseGod.on200() }
+    )
+}
