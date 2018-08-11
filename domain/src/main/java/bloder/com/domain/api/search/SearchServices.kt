95686bcc64c71b1f2f6aaa97ae57ce14a0dacd6e
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-11 09:56:15 -0300

    Change wrong model returning in search services

--- domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
+++ domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
@@ -1,11 +1,11 @@
 package bloder.com.domain.api.search
 
-import bloder.com.domain.models.Status
+import bloder.com.domain.payloads.SearchResponsePayload
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SearchServices {
 
-    @GET("tweets.json") fun searchTweets(@Query("q") name: String) : Single<List<Status>>
+    @GET("tweets.json") fun searchTweets(@Query("q") name: String) : Single<SearchResponsePayload>
 }
