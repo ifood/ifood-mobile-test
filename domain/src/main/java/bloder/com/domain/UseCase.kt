756e2010089f026eb84765b2b941bef43c725d4a
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-11 09:33:09 -0300

    Create tweet fetch api service

--- domain/src/main/java/bloder/com/domain/UseCase.kt
+++ domain/src/main/java/bloder/com/domain/UseCase.kt
@@ -2,8 +2,6 @@ package bloder.com.domain
 
 import bloder.com.domain.repository.RepositoryFactory
 import bloder.com.domain.repository.production.ProductionRepository
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
@@ -11,9 +9,7 @@ open class UseCase {
 
     private var repository: RepositoryFactory = ProductionRepository()
     private var disposables = CompositeDisposable()
-    private var observeScheduler: Scheduler? = AndroidSchedulers.mainThread()
 
-    fun getScheduler() : Scheduler = observeScheduler ?: AndroidSchedulers.mainThread()
 
     fun dispose() {
         disposables.clear()
--- domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
+++ domain/src/main/java/bloder/com/domain/api/search/SearchServices.kt
@@ -1,6 +1,11 @@
 package bloder.com.domain.api.search
 
+import bloder.com.domain.models.Status
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SearchServices {
 
+    @GET("tweets.json") fun searchTweets(@Query("q") name: String) : Single<List<Status>>
 }
