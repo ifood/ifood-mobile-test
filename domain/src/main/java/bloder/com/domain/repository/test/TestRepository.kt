0dc0eafe85fa9236b831b9625694c65dd9562c5f
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-11 09:58:02 -0300

    Implement search respository in test repo

--- domain/src/main/java/bloder/com/domain/repository/test/TestRepository.kt
+++ domain/src/main/java/bloder/com/domain/repository/test/TestRepository.kt
@@ -1,7 +1,10 @@
 package bloder.com.domain.repository.test
 
 import bloder.com.domain.repository.RepositoryFactory
+import bloder.com.domain.repository.resources.SearchRepository
+import bloder.com.domain.repository.test.resources.SearchRepositoryTest
 
 class TestRepository : RepositoryFactory {
 
+    override fun forSearch(): SearchRepository = SearchRepositoryTest()
 }
