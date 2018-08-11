ac826e9daf74c805385404908b9c601f23c864e6
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-11 09:57:30 -0300

    Implement search repository in production repo

--- domain/src/main/java/bloder/com/domain/repository/production/ProductionRepository.kt
+++ domain/src/main/java/bloder/com/domain/repository/production/ProductionRepository.kt
@@ -1,8 +1,10 @@
 package bloder.com.domain.repository.production
 
 import bloder.com.domain.repository.RepositoryFactory
+import bloder.com.domain.repository.production.resources.SearchRepositoryProduction
+import bloder.com.domain.repository.resources.SearchRepository
 
 class ProductionRepository : RepositoryFactory {
 
-
+    override fun forSearch(): SearchRepository = SearchRepositoryProduction()
 }
