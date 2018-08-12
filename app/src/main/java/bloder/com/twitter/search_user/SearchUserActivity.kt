e06739e93fe19990d24297cbf6b928dc20c0f776
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-12 13:45:25 -0300

    Create search tweets layout

--- app/src/main/java/bloder/com/twitter/search_user/SearchUserActivity.kt
+++ app/src/main/java/bloder/com/twitter/search_user/SearchUserActivity.kt
@@ -1,7 +1,10 @@
 package bloder.com.twitter.search_user
 
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import bloder.com.twitter.R
 
 
@@ -10,5 +13,13 @@ class SearchUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_user)
+        changeSystemBarColor()
     }
+
+    private fun changeSystemBarColor() = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        val window = window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+    } else {}
 }
