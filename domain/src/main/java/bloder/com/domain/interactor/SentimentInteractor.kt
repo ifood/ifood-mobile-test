618193272dc825e63a23ce80236b1c316197a5ce
Bloder <aptgetinstalldanielleme@gmail.com>
2018-08-13 09:36:33 -0300

    Create sentiment interactor test

--- domain/src/main/java/bloder/com/domain/interactor/SentimentInteractor.kt
+++ domain/src/main/java/bloder/com/domain/interactor/SentimentInteractor.kt
@@ -4,7 +4,7 @@ import bloder.com.domain.SingleUseCase
 import bloder.com.domain.binding.SingleUseCaseBinding
 import bloder.com.domain.models.sentiment.Sentiment
 
-class SentimentInteractor : SingleUseCase() {
+open class SentimentInteractor : SingleUseCase() {
 
     fun getSentimentFrom(tweet: String) : SingleUseCaseBinding<Sentiment> {
         return run(repository.forSentiment().getSentimentFor(tweet))
--- domain/src/test/java/bloder/com/domain/SentimentInteractorTest.kt
+++ domain/src/test/java/bloder/com/domain/SentimentInteractorTest.kt
@@ -1,8 +1,55 @@
 package bloder.com.domain
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import bloder.com.domain.interactor.SentimentInteractor
+import bloder.com.domain.models.sentiment.SENTIMENT
+import bloder.com.domain.models.sentiment.Sentiment
+import bloder.com.domain.repository.RepositoryFactory
+import bloder.com.domain.repository.resources.SentimentRepository
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class SentimentInteractorTest {
+
+    @get:Rule var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val interactor = mock<SentimentInteractor>()
+
+    @Before fun setup() {
+        interactor.test()
+    }
+
+    @Test fun completeGetTweetSentiment() {
+        val test = interactor.getSentimentFrom("test").subscribe {}.test()
+        test.assertComplete()
+    }
+
+    @Test fun returnFilledSentiment() {
+        val test = interactor.getSentimentFrom("test").subscribe {}.test()
+        test.assertValue { value ->
+            value.getSentiment() == SENTIMENT.SAD
+        }
+    }
+
+    @Test fun returnErrorWhenGetSentiment() {
+        val errorMessage = "A unknown problem has occurred"
+        val repository = mock<RepositoryFactory>()
+        val searchRepository = mock<SentimentRepository>()
+        interactor.testWith(repository)
+        whenever(repository.forSentiment()).thenReturn(searchRepository)
+        whenever(searchRepository.getSentimentFor(any())).thenReturn(Single.create<Sentiment> {
+            it.onError(Throwable(errorMessage))
+        })
+        val test = interactor.getSentimentFrom("").subscribe {}.test()
+        test.assertError { error ->
+            error.message == errorMessage
+        }
+    }
 }
