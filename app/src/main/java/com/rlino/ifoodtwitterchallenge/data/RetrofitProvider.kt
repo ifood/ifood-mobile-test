5100823e98aecfa4c8cd5ee214096d66ae0f0692
Rodrigo Lino da Costa <rodrigolino.c@gmail.com>
2018-09-06 18:40:14 -0300

    Configurado Retrofit com logger; Adicinoado URL do Twitter na configuração da build;

--- app/src/main/java/com/rlino/ifoodtwitterchallenge/data/RetrofitProvider.kt
+++ app/src/main/java/com/rlino/ifoodtwitterchallenge/data/RetrofitProvider.kt
@@ -1,17 +1,36 @@
 package com.rlino.ifoodtwitterchallenge.data
 
+import com.rlino.ifoodtwitterchallenge.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 
 object RetrofitProvider {
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-                .baseUrl("https://api.twitter.com/1.1/")
+                .baseUrl(BuildConfig.TWITTER_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(OkHttpProvider.okHttpClient)
                 .build()
     }
 
     inline fun <reified T> create(): T {
         return retrofit.create(T::class.java)
+    }
+
+}
+
+object OkHttpProvider {
+
+    val okHttpClient by lazy {
+        OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor(HttpLoggingInterceptor.Logger {
+                  Timber.tag("OkHttp").d(it)
+                }))
+                .build()
     }
 
 }
