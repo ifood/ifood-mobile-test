e4858f2ae3f8f668ca34cd5ba8f93f34d4cc42a8
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-09-07 17:11:34 -0300

    Requesting tweets using a username

--- app/src/main/java/br/com/fornaro/tweetssentiment/repository/TweetsRepository.kt
+++ app/src/main/java/br/com/fornaro/tweetssentiment/repository/TweetsRepository.kt
@@ -3,25 +3,56 @@ package br.com.fornaro.tweetssentiment.repository
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import br.com.fornaro.tweetssentiment.api.NaturalLanguageApi
-import br.com.fornaro.tweetssentiment.model.*
+import br.com.fornaro.tweetssentiment.api.TweeterApi
+import br.com.fornaro.tweetssentiment.model.Document
+import br.com.fornaro.tweetssentiment.model.SentimentRequest
+import br.com.fornaro.tweetssentiment.model.SentimentResponse
+import br.com.fornaro.tweetssentiment.model.Tweet
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer
+import se.akerfeldt.okhttp.signpost.SigningInterceptor
 
 class TweetsRepository {
 
     fun getTweets(username: String): LiveData<List<Tweet>>? {
         val data = MutableLiveData<List<Tweet>>()
 
-        val list = mutableListOf<Tweet>()
-        list.add(Tweet(1, "This is a sad tweet"))
-        list.add(Tweet(2, "This is a newtral tweet"))
-        list.add(Tweet(3, "This is a happy tweet"))
-        data.value = list
+        val api = Retrofit.Builder()
+                .baseUrl("https://api.twitter.com/")
+                .client(getRequestClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(TweeterApi::class.java)
+
+        api.getTweets(username)
+                .enqueue(object : Callback<List<Tweet>> {
+                    override fun onFailure(call: Call<List<Tweet>>?, t: Throwable?) {
+                    }
+
+                    override fun onResponse(call: Call<List<Tweet>>?, response: Response<List<Tweet>>?) {
+                        if (response?.isSuccessful!!) {
+                            data.value = response.body()
+                        }
+                    }
+                })
 
         return data
+    }
+
+    private fun getRequestClient(): OkHttpClient {
+        val consumer = OkHttpOAuthConsumer("DLvdcgSJ1fdA3TSnSrQLVInVP",
+                "YA6ZNKsFIwswFxvgBf0jzF3D0gpdFUcxSzod9KrkFvOhoLaD1X")
+        consumer.setTokenWithSecret("1000221381454376962-G3SwGQWvB18gN4EljJcSLWvqCpobiy",
+                "FMr04toyVx3auYd3PvFu1MsOcdKt2PewPDsZSRihpSSSp")
+
+        return OkHttpClient.Builder()
+                .addInterceptor(SigningInterceptor(consumer))
+                .build()
     }
 
     fun analyzeTweet(tweet: Tweet): LiveData<Tweet> {
