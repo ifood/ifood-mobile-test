ce29d024ee988e2a5a542f4a211d433fb5c37b2e
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-09-07 14:28:04 -0300

    Executing seeTweets from viewModel when click on Done keyboard button on MainActivity

--- app/src/main/java/br/com/fornaro/tweetssentiment/view/main/MainActivity.kt
+++ app/src/main/java/br/com/fornaro/tweetssentiment/view/main/MainActivity.kt
@@ -3,14 +3,17 @@ package br.com.fornaro.tweetssentiment.view.main
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import br.com.fornaro.tweetssentiment.R
 import br.com.fornaro.tweetssentiment.common.AppConstants
 import br.com.fornaro.tweetssentiment.databinding.ActivityMainBinding
 import br.com.fornaro.tweetssentiment.view.tweets.TweetsActivity
 import br.com.fornaro.tweetssentiment.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +24,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setupDoneKeyboardAction()
         setupViewModel()
         assignViewModel(viewModel)
+    }
+
+    private fun setupDoneKeyboardAction() {
+        usernameText.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                viewModel.seeTweetsButtonClick(v.text.toString())
+            }
+            false
+        }
     }
 
     private fun setupViewModel() {
