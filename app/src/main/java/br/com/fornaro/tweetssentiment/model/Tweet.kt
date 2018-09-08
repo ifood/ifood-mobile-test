504d065b4f27cf7f194c3809467d3bc689e107f7
Douglas Fornaro <douglasfornaro@hotmail.com>
2018-09-08 16:48:50 -0300

    Refactoring some methods position

--- app/src/main/java/br/com/fornaro/tweetssentiment/model/Tweet.kt
+++ app/src/main/java/br/com/fornaro/tweetssentiment/model/Tweet.kt
@@ -1,30 +1,9 @@
 package br.com.fornaro.tweetssentiment.model
 
-import android.content.Context
-import android.support.v4.content.ContextCompat
-import br.com.fornaro.tweetssentiment.R
 import java.util.*
 
 data class Tweet(
         val text: String,
         val createdAt: Date,
-        var sentiment: Sentiment? = null
-) {
-    fun getSentimentText(context: Context): String {
-        return when (sentiment) {
-            Sentiment.Happy -> context.getString(R.string.happy, "\ud83d\ude03")
-            Sentiment.Neutral -> context.getString(R.string.neutral, "\ud83d\ude10")
-            Sentiment.Sad -> context.getString(R.string.sad, "\ud83d\ude14")
-            else -> ""
-        }
-    }
-
-    fun getSentimentColor(context: Context): Int {
-        return when (sentiment) {
-            Sentiment.Happy -> ContextCompat.getColor(context, R.color.happy)
-            Sentiment.Neutral -> ContextCompat.getColor(context, R.color.neutral)
-            Sentiment.Sad -> ContextCompat.getColor(context, R.color.sad)
-            else -> 0
-        }
-    }
-}
+        var sentiment: Sentiment = Sentiment.None
+)
