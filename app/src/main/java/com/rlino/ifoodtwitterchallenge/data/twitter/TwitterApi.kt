0244e54640c0263fd18dfdf692f6b179bd21b3d8
Rodrigo Lino da Costa <rodrigolino.c@gmail.com>
2018-09-10 22:03:50 -0300

    Adicionado arquivo para configuração de chaves

--- app/src/main/java/com/rlino/ifoodtwitterchallenge/data/twitter/TwitterApi.kt
+++ app/src/main/java/com/rlino/ifoodtwitterchallenge/data/twitter/TwitterApi.kt
@@ -11,7 +11,8 @@ interface TwitterApi {
     @GET("${BuildConfig.TWITTER_API_VERSION}/statuses/user_timeline.json")
     fun getTweetsFromUser(@Query("screen_name") from: String,
                           @Query("exclude_replies") excludeReplies: Boolean = true,
-                          @Query("include_rts") includeRts: Boolean = false
+                          @Query("include_rts") includeRts: Boolean = false,
+                          @Query("count") count: Int = 50
     ): Single<List<Tweet>>
 
     @POST("oauth2/token")
