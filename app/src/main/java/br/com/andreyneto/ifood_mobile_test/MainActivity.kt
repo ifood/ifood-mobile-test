8f1641a22817ef009ab9df282bba746edb29dfa3
Andrey Neto <andrey@andreyneto.com.br>
2018-09-27 21:32:56 -0300

    Layouts created

--- app/src/main/java/br/com/andreyneto/ifood_mobile_test/MainActivity.kt
+++ app/src/main/java/br/com/andreyneto/ifood_mobile_test/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.andreyneto.ifood_mobile_test
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
     }
 }
