456c822b300313a153aaa346521642a0eecfa894
Thales Frigo <thalesfrigo@gmail.com>
2018-11-27 06:45:46 -0200

    Initial commit

---
+++ SentimentAnalysis/SentimentAnalysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SentimentAnalysis
+//
+//  Created by Thales Frigo on 27/11/18.
+//  Copyright © 2018 Thales Frigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
