a0930d1754b806110736331a327423ea217dbf8a
gabriel.catice@gmail.com <gabriel.catice@gmail.com>
2018-11-28 17:19:33 -0200

    dependency injection adjustments

--- my-tweets/Application/Dependency Injection/DependyInjection.swift
+++ my-tweets/Application/Dependency Injection/DependyInjection.swift
@@ -29,8 +29,6 @@ enum HomePresenterConfigurator {
 // DataSource Dependency Injection
 enum DataSourceConfigurator {
     static func setup(with container: Container) {
-        
-//        container.autoregister(TwitterDataSource.self, initializer: TwitterDataSource.init)
         container.autoregister(SentimentDataSource.self, initializer: SentimentDataSource.init)
     }
 }
