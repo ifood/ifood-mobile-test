eb1fdd65b4d16e6772d933e4e676979eaee76c72
silvio.pereira <silvioper@gmail.com>
2018-12-02 23:34:14 -0200

    Initial project creation
    - Simple project created by Android Studio
    - Creation of domain and data modules
    - Added custom icons
    - Customized gitignore
    - Added empty Application class

---
+++ app/src/main/java/com/shrpereira/tweetsense/app/TweetSenseApplication.kt
@@ -0,0 +1,8 @@
+@file:Suppress("unused")
+
+package com.shrpereira.tweetsense.app
+
+import android.app.Application
+
+class TweetSenseApplication : Application() {
+}
---
+++ app/src/main/java/com/shrpereira/tweetsense/app/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.shrpereira.tweetsense.app.ui.splash
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.shrpereira.tweetsense.app.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
