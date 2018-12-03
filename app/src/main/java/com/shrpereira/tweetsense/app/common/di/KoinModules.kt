8cd2ac93fc2b5a35ec82027f5b29e68861bc824f
silvio.pereira <silvioper@gmail.com>
2018-12-03 00:57:20 -0200

    Adding initial configuration for Koin dependency injection

---
+++ app/src/main/java/com/shrpereira/tweetsense/app/common/di/KoinModules.kt
@@ -0,0 +1,15 @@
+package com.shrpereira.tweetsense.app.common.di
+
+import org.koin.dsl.module.module
+
+val uiModule = module(override = true) {
+
+}
+
+val domainModule = module(override = true) {
+
+}
+
+val dataModule = module(override = true) {
+
+}
