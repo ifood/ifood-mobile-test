86169bfd144d83a80caadb9811b07d134602eea5
silvio.pereira <silvioper@gmail.com>
2018-12-10 02:30:07 -0200

    Creating RetrofitBuilder to be used for external network calls

--- app/src/main/java/com/shrpereira/tweetsense/app/common/di/KoinModules.kt
+++ app/src/main/java/com/shrpereira/tweetsense/app/common/di/KoinModules.kt
@@ -1,7 +1,9 @@
 package com.shrpereira.tweetsense.app.common.di
 
+import com.shrpereira.tweetsense.app.BuildConfig
 import com.shrpereira.tweetsense.data.cache.authentication.AccessTokenDataSource
 import com.shrpereira.tweetsense.data.cache.authentication.AccessTokenDataSourceImpl
+import com.shrpereira.tweetsense.data.remote.RetrofitBuilder
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
 
@@ -14,6 +16,24 @@ val domainModule = module(override = true) {
 }
 
 val dataModule = module(override = true) {
+	factory("TwitterRetrofitBuilder") {
+		RetrofitBuilder(
+			BuildConfig.TWITTER_API_URL,
+			androidContext().cacheDir,
+			RetrofitBuilder.ApiType.TwitterAPI(
+				BuildConfig.TWITTER_API_KEY,
+				BuildConfig.TWITTER_API_SECRET_KEY,
+				get()
+			)
+		)
+	}
+	factory("GoogleRetrofitBuilder") {
+		RetrofitBuilder(
+			BuildConfig.GOOGLE_API_URL,
+			androidContext().cacheDir,
+			RetrofitBuilder.ApiType.GoogleAPI(get())
+		)
+	}
 
 	single { AccessTokenDataSourceImpl(androidContext()) as AccessTokenDataSource }
 }
