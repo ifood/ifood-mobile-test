2cacd429abede7568593398de54ff57ddbeb1228
silvio.pereira <silvioper@gmail.com>
2018-12-10 02:06:29 -0200

    Converting Glide module to kotlin

--- app/src/main/java/com/shrpereira/tweetsense/app/common/glide/CustomGlideModule.java
+++
@@ -1,8 +0,0 @@
-package com.shrpereira.tweetsense.app.common.glide;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class CustomGlideModule extends AppGlideModule {
-}
---
+++ app/src/main/java/com/shrpereira/tweetsense/app/common/glide/CustomGlideModule.kt
@@ -0,0 +1,7 @@
+package com.shrpereira.tweetsense.app.common.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class CustomGlideModule : AppGlideModule()
