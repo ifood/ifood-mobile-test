e4bb42f462b55fae382f496e7ba9ab04a727f68e
Jean Vinge <jean.vinge@gmail.com>
2018-12-24 14:30:56 -0200

    adding gitignore

---
+++ ifood-mobile-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ifood-mobile-test
+//
+//  Created by Jean Vinge on 24/12/18.
+//  Copyright © 2018 Jean Vinge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ifood-mobile-testTests/ifood_mobile_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  ifood_mobile_testTests.swift
+//  ifood-mobile-testTests
+//
+//  Created by Jean Vinge on 24/12/18.
+//  Copyright © 2018 Jean Vinge. All rights reserved.
+//
+
+import XCTest
+@testable import ifood_mobile_test
+
+class ifood_mobile_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
