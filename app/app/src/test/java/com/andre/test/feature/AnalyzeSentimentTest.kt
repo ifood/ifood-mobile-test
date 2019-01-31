996b361a4fbd5bb3884fd08645c552014b59e2a6
andrechiose <dehhht@gmail.com>
2019-01-31 17:29:01 -0200

    Start testing AnalyzeSentiment class

--- app/app/src/test/java/com/andre/test/feature/AnalyzeSentimentTest.kt
+++ app/app/src/test/java/com/andre/test/feature/AnalyzeSentimentTest.kt
@@ -1,2 +1,55 @@
 package com.andre.test.feature
 
+import com.andre.test.BaseUnitTest
+import com.andre.test.core.network.NetworkResponse
+import com.andre.test.features.AnalyzeSentiment
+import com.andre.test.mock.MockedObjects
+import com.google.api.services.language.v1.CloudNaturalLanguage
+import com.google.api.services.language.v1.model.*
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+
+class AnalyzeSentimentTest: BaseUnitTest() {
+
+    private lateinit var analyzeSentiment: AnalyzeSentiment
+
+    @Mock private lateinit var naturalLanguage: CloudNaturalLanguage
+    @Mock private lateinit var annotateTextResponse: AnnotateTextResponse
+
+
+    @Before fun setUp() {
+        analyzeSentiment = AnalyzeSentiment(naturalLanguage)
+
+        given { naturalLanguage.documents().annotateText(createAnnotateTextRequest()).execute() }.willReturn(annotateTextResponse)
+//        given { annotateTextResponse.documentSentiment }.willReturn(sentiment)
+
+    }
+
+    @Test fun `should call Cloud Natural Language execute`() {
+        runBlocking {
+            val networkResponse = analyzeSentiment.run(AnalyzeSentiment.Params(MockedObjects.tweetMessage))
+            assert(networkResponse is NetworkResponse.Success<*>)
+        }
+
+    }
+
+    private fun createAnnotateTextRequest(): AnnotateTextRequest {
+        val document = Document()
+        document.type = "PLAIN_TEXT"
+        document.content = MockedObjects.tweetMessage
+
+        val features = Features()
+        features.extractDocumentSentiment = true
+
+        val request = AnnotateTextRequest()
+        request.document = document
+        request.features = features
+
+        return request
+    }
+}
---
+++ app/app/src/test/java/com/andre/test/mock/MockedObjects.kt
@@ -0,0 +1,7 @@
+package com.andre.test.mock
+
+object MockedObjects {
+
+    val tweetMessage = "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  " +
+            "Sundar Pichai said in his keynote that users love their new Android phones."
+}
