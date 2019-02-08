39afe5dda349207c63df07cd3723e466526b1e11
Marcus Souza <marcus.souza@concrete.com.br>
2019-02-08 00:26:05 -0200

    Trata erro de internet

--- app/src/main/java/movile/marcus/com/br/moviletest/util/HandlerFlowable.kt
+++ app/src/main/java/movile/marcus/com/br/moviletest/util/HandlerFlowable.kt
@@ -1,9 +1,11 @@
 package movile.marcus.com.br.moviletest.util
 
 import android.arch.lifecycle.MutableLiveData
-import movile.marcus.com.br.moviletest.ui.BaseViewModel
 import io.reactivex.Flowable
 import movile.marcus.com.br.moviletest.model.data.Resource
+import movile.marcus.com.br.moviletest.model.data.Status
+import movile.marcus.com.br.moviletest.ui.BaseViewModel
+import java.io.IOException
 
 fun <T> Flowable<T>.toHandlerFlowable() = HandlerFlowable(this)
 
@@ -20,6 +22,10 @@ data class HandlerFlowable<T>(val observable: Flowable<T>) {
     }
 
     private fun handleError(liveData: MutableLiveData<Resource<T>>, it: Throwable) {
-        liveData.postValue(Resource.error(it))
+        if (it is IOException) {
+            liveData.postValue(Resource.error(it, Status.INTERNET_ERROR))
+        } else {
+            liveData.postValue(Resource.error(it))
+        }
     }
 }
