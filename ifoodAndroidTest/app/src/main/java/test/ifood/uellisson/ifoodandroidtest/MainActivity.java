3433b30231cb5da17acddb746e6a583bfe30bbae
uellisson lopes <uellisson.silva@e-deploy.com>
2019-02-23 10:55:54 -0300

    Initial commit

---
+++ ifoodAndroidTest/app/src/main/java/test/ifood/uellisson/ifoodandroidtest/MainActivity.java
@@ -0,0 +1,13 @@
+package test.ifood.uellisson.ifoodandroidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
