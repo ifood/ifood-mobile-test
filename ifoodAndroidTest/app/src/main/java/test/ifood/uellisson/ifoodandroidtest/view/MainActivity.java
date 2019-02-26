df2f6608bccd49561f36efcfe7dea625108fa331
uellisson lopes <uellisson.silva@e-deploy.com>
2019-02-26 20:03:12 -0300

    implementing tweet search by keyboard

--- ifoodAndroidTest/app/src/main/java/test/ifood/uellisson/ifoodandroidtest/view/MainActivity.java
+++ ifoodAndroidTest/app/src/main/java/test/ifood/uellisson/ifoodandroidtest/view/MainActivity.java
@@ -1,14 +1,20 @@
 package test.ifood.uellisson.ifoodandroidtest.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
 import butterknife.Bind;
@@ -29,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements TweetsPresenter.V
     @Bind(R.id.loading)
     ProgressBar loading;
 
+    @Bind(R.id.search_button)
+    ImageButton searchButton;
+
     private TweetAdapter tweetAdapter;
     LinearLayoutManager layoutManager;
     DividerItemDecoration itemDecor;
@@ -44,12 +53,30 @@ public class MainActivity extends AppCompatActivity implements TweetsPresenter.V
 
     @OnClick (R.id.search_button)
     public void searchTwittes(){
+        searchTweetsByUserName(twitterUserName.getText().toString());
+    }
+
+    @OnClick ({R.id.twitter_username})
+    public void searchTwittesKeyboard(){
+        twitterUserName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                   searchTweetsByUserName(twitterUserName.getText().toString());
+                   hideKeyBoard(v);
+                }
+                return false;
+            }
+        });
+    }
+
+    public void searchTweetsByUserName(String username) {
         tweetsPresenter = new TweetsPresenter();
         tweetsPresenter.setView(this);
         if (twitterUserName.getText().toString().isEmpty()) {
             Toast.makeText(getApplicationContext(), getString(R.string.empty_userName), Toast.LENGTH_LONG).show();
         } else {
-            tweetsPresenter.initialize(twitterUserName.getText().toString().replace(" ", ""));
+            tweetsPresenter.initialize(username.replace(" ", ""));
         }
     }
 
@@ -78,10 +105,17 @@ public class MainActivity extends AppCompatActivity implements TweetsPresenter.V
     @Override
     public void hideLoading() {
         loading.setVisibility(View.GONE);
+        searchButton.setEnabled(true);
     }
 
     @Override
     public void showErrorMessage(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        searchButton.setEnabled(false);
+    }
+
+    public static void hideKeyBoard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
