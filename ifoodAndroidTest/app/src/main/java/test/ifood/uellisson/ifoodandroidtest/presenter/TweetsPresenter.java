497090f81dd8ac854ed54ae1ccb60883899b73ea
uellisson lopes <uellisson.silva@e-deploy.com>
2019-02-26 12:17:34 -0300

    implementing request Twitter API in presenter

--- ifoodAndroidTest/app/src/main/java/test/ifood/uellisson/ifoodandroidtest/presenter/TweetsPresenter.java
+++ ifoodAndroidTest/app/src/main/java/test/ifood/uellisson/ifoodandroidtest/presenter/TweetsPresenter.java
@@ -1,16 +1,32 @@
 package test.ifood.uellisson.ifoodandroidtest.presenter;
 
+import android.util.Base64;
+import android.util.Log;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import test.ifood.uellisson.ifoodandroidtest.BuildConfig;
+import test.ifood.uellisson.ifoodandroidtest.data.model.AuthorizationToken;
+import test.ifood.uellisson.ifoodandroidtest.data.network.TwitterAPI;
+import test.ifood.uellisson.ifoodandroidtest.data.network.TwitterApiProvide;
+import test.ifood.uellisson.ifoodandroidtest.data.model.TweetEntity;
 import test.ifood.uellisson.ifoodandroidtest.model.Tweet;
 
 public class TweetsPresenter extends Presenter<TweetsPresenter.View>{
     List<Tweet> lt;
+    final TwitterAPI twitterAPI;
+
+    public TweetsPresenter() {
+       twitterAPI = TwitterApiProvide.provideAndCreate();
+    }
 
     @Override
     public void initialize() {
         super.initialize();
+        requestAccessToken();
         //TODO implements request API Twitter
         getView().showLoading();
         Tweet t1 = new Tweet("mensagem 1", "hoje");
@@ -26,5 +42,42 @@ public class TweetsPresenter extends Presenter<TweetsPresenter.View>{
 
     public interface View extends Presenter.BaseView {
         void showTwittes (List<Tweet> tweetList);
+    }
+
+    private void requestAccessToken() {
+        twitterAPI.getAccessToken(getAuthorizationHeader(), "client_credentials").enqueue(new Callback<AuthorizationToken>() {
+            @Override
+            public void onResponse(Call<AuthorizationToken> call, Response<AuthorizationToken> response) {
+                String token = "Bearer " + response.body().getAccessToken();
+                twitterAPI.getTweetsByUsername(token, "uellissonlopes", 2).enqueue(new Callback<List<TweetEntity>>() {
+                    @Override
+                    public void onResponse(Call<List<TweetEntity>> call, Response<List<TweetEntity>> response) {
+                        response.body();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<TweetEntity>> call, Throwable t) {
+                        Log.e("Error", t.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<AuthorizationToken> call, Throwable t) {
+                Log.e("Error", t.getMessage());
+            }
+        });
+    }
+
+    private String getAuthorizationHeader() {
+        try {
+            String consumerKeyAndSecret = BuildConfig.API_KEY_TWITTER + ":" + BuildConfig.API_SECRET_KEY_TWITTER;
+            byte[] data = consumerKeyAndSecret.getBytes("UTF-8");
+            String base64 = Base64.encodeToString(data, Base64.NO_WRAP);
+
+            return "Basic " + base64;
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
     }
 }
