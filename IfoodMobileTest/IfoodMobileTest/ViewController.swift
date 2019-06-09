7ade5d3fef9e97d18dc1de389032c727fbbb0f35
Andre Vieira <vieira.agp@gmail.com>
2019-06-08 22:11:38 -0300

    create project

---
+++ IfoodMobileTest/IfoodMobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IfoodMobileTest
+//
+//  Created by André Vieira on 08/06/19.
+//  Copyright © 2019 André Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
